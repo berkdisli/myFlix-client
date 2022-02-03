4edc16f8f495eb69b693f2e53b74aa3c3a9d9d27
Berk Disli <berkdisli@yahoo.com>
2022-02-03 15:33:10 +0100

    main view :username

--- dist/index.e1b27ffe.js
+++ dist/index.e1b27ffe.js
@@ -25940,7 +25940,7 @@ class MainView extends _reactDefault.default.Component {
                                         }),
                                         /*#__PURE__*/ _jsxRuntime.jsx(_reactBootstrap.Nav.Link, {
                                             className: "navbar-link",
-                                            href: `/users/${user1}`,
+                                            href: `/users/:username`,
                                             __source: {
                                                 fileName: "src/components/main-view/main-view.jsx",
                                                 lineNumber: 100,
